89ca7a74e97d19050f873298a0ae5553485ad3c1
andriiannsyah <anandri394@gmail.com>
2024-06-20 11:20:07 +0700

    add : 9-api-routes

--- learn-next/pages/index.js
+++ learn-next/pages/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/Layouts";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log("Error => ", err));
+  }, []);
   return (
     <>
       <Layout metaTitle="Home" metasDescription="Ini meta deskripsi dari menu Home">
